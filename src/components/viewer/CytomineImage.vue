1e9474b01cdfc71210e3a327a7d9ab3c43fcd2cf
jiang-eqx <jiang@eqx.vc>
2023-09-13 12:21:02 +0800

    synchronize the annotation line color during init

--- src/components/viewer/CytomineImage.vue
+++ src/components/viewer/CytomineImage.vue
@@ -508,6 +508,7 @@ export default {
   watch: {
     viewState() {
       this.savePosition();
+      this.$store.dispatch(this.imageModule + 'updatedAnnotationColor');
     },
     overviewCollapsed(value) {
       this.$store.commit(this.imageModule + 'setOverviewCollapsed', value);
